658c987468a2ece01ce79ea3e430b11f6fec3d28
Lucas Charrone Catozo <lucascatozo@outlook.com>
2025-10-27 21:15:39 -0300

    Delete Trabalho marcio directory

--- Trabalho marcio/script.js
+++
@@ -1,6 +0,0 @@
-const blendLayer = document.getElementById('blend');
-const select = document.getElementById('blendMode');
-
-select.addEventListener('change', () => {
-  blendLayer.style.mixBlendMode = select.value;
-});
